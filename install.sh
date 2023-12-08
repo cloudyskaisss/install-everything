ea79a5e317f5d839a90b0b807dad1b09cc96f30b
cloudyskaisss <146793021+cloudyskaisss@users.noreply.github.com>
2023-12-08 00:26:59 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,20 @@ if [ $(id -u) -ne 0 ]
   then echo "***THIS SCRIPT MUST BE RUN AS ROOT***"
   exit
 fi
-
+apt update
 apt install cowsay cmatrix sl figlet fortune libaa-bin rig toilet lolcat -y
 
 git clone https://github.com/Z4nzu/hackingtool
 cd hackingtool
 echo "1
 " | bash install.sh
+
+sudo dpkg --add-architecture i386
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources
+sudo apt update
+sudo apt install --install-recommends winehq-stable
 
 curl "https://raw.githubusercontent.com/cloudyskaisss/asciiquarium-install/main/install.sh" | bash
 
